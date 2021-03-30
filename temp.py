1cb3c92e2196396b84caf0571fff717d3a2af6f9
ggu1012 <ggu1012@naver.com>
2021-03-30 16:25:23 +0900

    nn

--- temp.py
+++ temp.py
@@ -80,6 +80,7 @@ def evaluate_state_value_by_matrix_inversion(env, discount=1.0):
     R = R[1:-1, :]
 
     # Transition matrix T
+
     T = np.zeros([WIDTH,  HEIGHT, WIDTH, HEIGHT])
     for i in range(WIDTH):
         for j in range(HEIGHT):
@@ -88,15 +89,20 @@ def evaluate_state_value_by_matrix_inversion(env, discount=1.0):
                     [i, j], ACTIONS[action])
                 T[i, j, next_i, next_j] += ACTION_PROB
 
+    # Flattens T matrix into [-1,1] shape.
     T = T.flatten()
+    
+    # Reshape flattened matrix into [16,16]
+    # Row has the information of [v(0), v(1), v(2), ..., v(15)]
     T = T.reshape([WIDTH*HEIGHT,WIDTH*HEIGHT])
+
+
     T = T[1:-1, 1:-1]
 
     iteration = 1000
 
        
-    # Identity matrix for I
-    I = np.identity(WIDTH*HEIGHT-2)
+
 
     # Initial V matrix = 0s
     V = np.zeros([14,1])
@@ -104,6 +110,12 @@ def evaluate_state_value_by_matrix_inversion(env, discount=1.0):
     # V = R + discount*T*V
     for i in range(iteration):
         V = R + np.matmul(T, V)
+    
+    # ... or V = inv(I-T)*R
+    # Identity matrix for I
+    I = np.identity(WIDTH*HEIGHT-2)
+    # V = np.matmul(np.linalg.inv(I-T), R)
+
     
     V = V.flatten()
     V = np.concatenate(([0], V, [0]))
